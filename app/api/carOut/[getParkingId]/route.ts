53f108fe910334ccf0826811a74a1aa6923b0cb7
Theerapath Ch <theerapath.dev@gmail.com>
2026-04-19 17:39:13 +0700

    create api  for car out

---
+++ app/api/carOut/[getParkingId]/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse, NextRequest } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function GET(_: NextRequest,
+   { params }: { params: { getParkingId: string } }) {
+   try {
+      const { getParkingId } = await params
+      //console.log(getParkingId);
+      const data = await prisma.parking.findUnique({
+         where: {
+            id: getParkingId
+         },
+         include: {
+            receipt: {
+               select: {
+                  id: true
+               }
+            }
+         }
+      })
+      // console.log(data);
+
+      if (!data) {
+         return NextResponse.json(
+            { success: false, message: 'Not found' },
+            { status: 404 }
+         )
+      }
+      return NextResponse.json({
+         success: true,
+         message: data,
+         status: 200
+      })
+
+   } catch (error) {
+      console.log(error);
+      return NextResponse.json(
+         { success: false, message: 'Internal Server Error' },
+         { status: 500 }
+      )
+   }
+}
---
+++ app/api/carOut/updateTimeOut/[putParkingId]/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+
+export async function PUT(req: NextRequest,
+    { params }: { params: { putParkingId: string } }) {
+    try {
+        const { putParkingId } = await params
+        //console.log(putParkingId);
+
+        const existing = await prisma.parking.findUnique({
+            where: { id: putParkingId }
+        })
+        //console.log(existing);
+        
+        if (!existing) {
+            return NextResponse.json(
+                { success: false, message: 'Not found' },
+                { status: 404 }
+            )
+        }
+
+        const now = new Date(Date.now() + 7 * 60 * 60 * 1000)
+        //console.log("time : ", now);
+
+        const updated = await prisma.parking.update({
+            where: {
+                id: putParkingId
+            },
+            data: {
+                out_at: now
+            }
+        })
+        //console.log(updated);
+        
+
+        return NextResponse.json(
+            { success: true, data: updated },
+            { status: 200 }
+        )
+
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json(
+            { success: false, message: 'Internal Server Error' },
+            { status: 500 }
+        )
+    }
+}
