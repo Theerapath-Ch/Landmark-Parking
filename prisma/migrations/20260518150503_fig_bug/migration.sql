6a2bfc1d42e9a8a60a87eb2221cc4b7ccd8466c7
Theerapath Ch <theerapath.dev@gmail.com>
2026-05-18 23:30:09 +0700

    add column db

---
+++ prisma/migrations/20260518150503_fig_bug/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "admin" ALTER COLUMN "name" DROP NOT NULL,
+ALTER COLUMN "lastname" DROP NOT NULL;
---
+++ prisma/migrations/20260518152420_change_type_of_username/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "admin" ALTER COLUMN "username" SET DATA TYPE TEXT;
